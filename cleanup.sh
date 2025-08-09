41a82a52b8045f8479198fdcbfc612889c6e40d8
RikiLaNeko <arthur.mortreux@lyceeastier.com>
2025-08-09 12:59:17 +0200

    MAJ: cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Exit immediately if a command exits with a non-zero status.
 set -e
 
-rm boot1.bin boot2.bin boot_x86.com os.img
+echo "Cleaning up build artifacts..."
+rm -f boot1.bin boot2.bin boot_x86.com os.img qemu.log
+echo "Cleanup complete."
